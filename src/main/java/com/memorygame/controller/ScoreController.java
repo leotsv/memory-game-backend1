4cb836890ec01c3a0afb03f1737afacf043d16fb
leotsv <github@leonid.com>
2025-02-20 22:27:01 -0500

    commit before deploy on Thu, Feb 20, 2025 10:26:59 PM

--- src/main/java/com/memorygame/controller/ScoreController.java
+++ src/main/java/com/memorygame/controller/ScoreController.java
@@ -2,31 +2,51 @@ package com.memorygame.controller;
 
 import com.memorygame.model.Score;
 import com.memorygame.repository.ScoreRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/scores")
 @CrossOrigin(origins = "*")
 public class ScoreController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(ScoreController.class);
     
     @Autowired
     private ScoreRepository scoreRepository;
 
-    @PostMapping
-    public ResponseEntity<Score> saveScore(@RequestBody Score score) {
-        score.setPlayedAt(LocalDateTime.now());
-        Score savedScore = scoreRepository.save(score);
-        return ResponseEntity.ok(savedScore);
+    @GetMapping("/")
+    public String home() {
+        logger.info("Accessing home endpoint");
+        return "Memory Game Backend is running!";
     }
 
-    @GetMapping("/top")
-    public ResponseEntity<List<Score>> getTopScores() {
-        List<Score> topScores = scoreRepository.findTop10ByOrderByTurnsAscTimeInSecondsAsc();
-        return ResponseEntity.ok(topScores);
+    @GetMapping("/api/scores")
+    public List<Score> getTopScores() {
+        logger.info("Fetching top scores");
+        try {
+            List<Score> scores = scoreRepository.findTop10ByOrderByTurnsAscTimeInSecondsAsc();
+            logger.info("Found {} top scores", scores.size());
+            return scores;
+        } catch (Exception e) {
+            logger.error("Error fetching top scores", e);
+            throw e;
+        }
+    }
+
+    @PostMapping("/api/scores")
+    public Score saveScore(@RequestBody Score score) {
+        logger.info("Saving new score for player: {}", score.getPlayerName());
+        try {
+            Score savedScore = scoreRepository.save(score);
+            logger.info("Successfully saved score with ID: {}", savedScore.getId());
+            return savedScore;
+        } catch (Exception e) {
+            logger.error("Error saving score for player: {}", score.getPlayerName(), e);
+            throw e;
+        }
     }
 } 
